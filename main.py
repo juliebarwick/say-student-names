8695a47576f72f98560c69065fd7474f0d67f400
Julie Barwick <julie@juliebarwick.com>
2021-09-17 15:28:43 -0700

    Make function to get classnames based on csv file names and use in user input plus small fixes

--- main.py
+++ main.py
@@ -2,25 +2,26 @@ import os
 import random
 import csv
 
-class_time = input("What class time is this: 1230 2 330 or 530?")
+def get_classnames(foldername):
+    class_names = []
+    class_files = [x[2] for x in os.walk(foldername)][0]
+    for file in class_files:
+        class_names.append(file.split('.')[0])
+    return ' '.join(class_names)
 
-if class_time == "1230":
-    class_file = 'csv/mus1names.csv'
+class_names = get_classnames('csv')
 
-if class_time == "2":
-    class_file = 'csv/mus2names.csv'
+selected_class = input("Enter the class (" + class_names + '): ')
 
-if class_time == "330":
-    class_file = 'csv/mus3names.csv'
-
-if class_time == "530":
-    class_file = 'csv/mus4names.csv'
+class_file = "csv/" + selected_class + ".csv"
 
 with open(class_file) as student_csv:
     student_csv_reader = csv.reader(student_csv)
     student_list = []
+
     # Skip header
     next(student_csv_reader)
+
     for row in student_csv_reader:
         student_list.append(' '.join(row))
 
@@ -33,4 +34,4 @@ while student_list:
     if user_input.lower() in quitting_words:
         break
 
-os.system("say 'All done'")
+os.system("say 'all done'")
