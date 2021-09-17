b60c7c0517b3d8c6623caab0d1b5b24d6a3b9a0a
Julie Barwick <julie@juliebarwick.com>
2021-09-17 15:46:44 -0700

    Fix inconsistent quote use

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ def get_classnames(foldername):
     class_names = []
     class_files = [x[2] for x in os.walk(foldername)][0]
     for file in class_files:
-        class_names.append(file.split('.')[0])
-    return ' '.join(class_names)
+        class_names.append(file.split(".")[0])
+    return " ".join(class_names)
 
 class_names = get_classnames('csv')
 
-selected_class = input("Enter the class (" + class_names + '): ')
+selected_class = input("Enter the class (" + class_names + "): ")
 
 class_file = "csv/" + selected_class + ".csv"
 
@@ -23,14 +23,14 @@ with open(class_file) as student_csv:
     next(student_csv_reader)
 
     for row in student_csv_reader:
-        student_list.append(' '.join(row))
+        student_list.append(" ".join(row))
 
 while student_list:
     student = random.choice(student_list)
     os.system("say " + student)
     student_list.remove(student)
     user_input = input("Press enter to continue (or 'q' to quit)...")
-    quitting_words = ['q', 'quit']
+    quitting_words = ["q", "quit"]
     if user_input.lower() in quitting_words:
         break
 
